2ec9bcca6fdddfe9fd699cbd2190e48990814ff1
ilhomxabibullayev <ilhomxabibullayev2004@gmail.com>
2024-09-13 08:43:47 +0500

    commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+let num = "123"
+console.log(Number.parseInt(num));
+
+num = "3.14"
+console.log(Number.parseFloat(num));
+
+num = "1234567891234567"
+console.log(Number.parseInt(num));
+
+num = "1010.2"
+console.log(Number.parseInt(num));
+
+num = 12345678901
+console.log(Number.MAX_VALUE);
+
+num = 4.5
+console.log(Number.isInteger(num));
+
+num = (Math.pow(2,53))
+console.log(Number.isSafeInteger(num));
+
+num = "345"
+console.log(Number.parseInt(num));
+
+num = ("abc123")
+console.log(Number.parseInt(num));
+
+num = ("123,8")
+console.log(Number.parseInt(num));
+
+num = 12345678901
+console.log(Number.MIN_VALUE);
+
+num = "56.78"
+console.log(Number.parseInt(num));
+
+num = "7.5"
+console.log(Math.round(num));
+
+num = "abc"
+console.log(Number.parseInt(num));
+
+num = "01234"
+console.log(Number.parseInt(num));
+
+num = 12345678901
+console.log(Number.MAX_SAFE_INTEGER);
+
+num = 12345678901
+console.log(Number.MIN_SAFE_INTEGER);
+
+num = "78.99"
+console.log(Math.floor(num));
